f1ac278a323d5a59f7682867d47fbe4b75f4f71b
Sanjeev <coolviccy123@gmail.com>
2024-08-08 02:55:53 +0530

    day-1-[progress added]

---
+++ Day-1/task.py
@@ -0,0 +1,14 @@
+# print("Hello\nworld!")
+# print("Please enter" + " " +"sanjeev")
+# input("What is your favorite")
+# print("Hello" + input("What is your name"))
+# print("Hello " + input("Hello Name!") + "!")
+
+# name = input("Enter your name: ")
+# print(name)
+
+name = "san jeev "
+print(len(name))
+name = "vicky"
+
+print(len(name))
