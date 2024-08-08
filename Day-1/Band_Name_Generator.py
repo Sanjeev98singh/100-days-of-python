01f8be491c2e323f8c4a7715a0feb0a42cfb4988
Sanjeev <coolviccy123@gmail.com>
2024-08-08 11:09:09 +0530

    band generator

---
+++ Day-1/Band_Name_Generator.py
@@ -0,0 +1,4 @@
+name = input("Welcome to the Band Name Generator\nWhat's the name of the city you grew up in?\n")
+pet_name = input("What's your pet name?\n")
+
+print("Your brand name could be " + name + " " + pet_name)
