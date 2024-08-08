890a3ed9133130f5463588561cc0844b89ab46b8
Sanjeev <coolviccy123@gmail.com>
2024-08-08 16:30:05 +0530

    list practice questions added

---
+++ Day-1/main.py
@@ -0,0 +1,15 @@
+# user_text = input("Please enter a todo\n")
+# print(user_text)
+# user_input = "Please enter a todo\n"
+# user_text = input(user_input)
+# print(user_text)
+user_input = "Please enter a todo\n"
+user_text1 = input(user_input)
+user_text2 = input(user_input)
+user_text3 = input(user_input)
+
+texts = [user_text1, user_text2, user_text3]
+
+print(texts)
+print(type(texts))
+
