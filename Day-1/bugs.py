9c4182fe03d5c1f2f497c203f2baaefb8025eadf
Sanjeev <coolviccy123@gmail.com>
2024-08-08 16:29:30 +0530

    bugs practice added

---
+++ Day-1/bugs.py
@@ -0,0 +1,8 @@
+# answers = ['yes', 'no', 'no', 'yes', 'no', 'yes', 'no', 'yes']
+# my_answer = input("What is your answer?")
+# answers = ['Yes', 'No', 'Yes', 'No', my_answer]
+# my_answer = input("What is your answer?")
+# answers = ['Yes', 'No', 'Yes', 'No', my_answer]
+
+my_answer = input("What is your answer?")
+answers = ['Yes', 'No', 'Yes', 'No', my_answer]
