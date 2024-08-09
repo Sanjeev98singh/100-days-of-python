cdfa484f665d5ab9a541c541f5c62d46210c8b8f
Sanjeev <coolviccy123@gmail.com>
2024-08-09 15:01:06 +0530

    task.py updated

--- Day-2/task.py
+++ Day-2/task.py
@@ -1,43 +1,43 @@
-print("Sanjeev"[6])
-print("Sanjeev"[-1])
+# print("Sanjeev"[6])
+# print("Sanjeev"[-1])
 
-print("123" + "456")
-print(123 + 456)
+# print("123" + "456")
+# print(123 + 456)
 
-large integers
-print(123_456_789)
+# large integers
+# print(123_456_789)
 
-float
-print(3.14536)
+# float
+# print(3.14536)
 
-boolean
+# boolean
 
-print(True)
+# print(True)
 
-print(False)
+# print(False)
 
-len("sanjeev")
+# len("sanjeev")
 
-print(type("sanjit"))
-print(type(3.14))
-print(type(3))
-print(type(True))
+# print(type("sanjit"))
+# print(type(3.14))
+# print(type(3))
+# print(type(True))
 
-TYPE CONVERSION
+# TYPE CONVERSION
 
-print(int("123") + int("456"))
+# print(int("123") + int("456"))
 
-print(int(True))
+# print(int(True))
 
-print("Number of letters in my name: " + str(len(input("Enter the name\n"))))
+# print("Number of letters in my name: " + str(len(input("Enter the name\n"))))
 
 
-print(5 // 3)
-print(5 / 3)
-print(5 ** 3)
+# print(5 // 3)
+# print(5 / 3)
+# print(5 ** 3)
 
-PEMDASLR - parenthesis,exponents,multiplication,/division and addition/substraction   
+# PEMDASLR - parenthesis,exponents,multiplication,/division and addition/substraction   
 
-print(3 * 3 + 3 / 3 - 3)
-print(3 * 3 + 3 // 3 - 3)
-print(3 * 3 + 3 // 3 - 7)
+# print(3 * 3 + 3 / 3 - 3)
+# print(3 * 3 + 3 // 3 - 3)
+# print(3 * 3 + 3 // 3 - 7)
