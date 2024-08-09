f144a7a58456b3a23f8786cf76a81fe2857aa651
Sanjeev <coolviccy123@gmail.com>
2024-08-09 15:18:33 +0530

    f strings updated

--- Day-2/bmi.py
+++ Day-2/bmi.py
@@ -1,8 +1,16 @@
-height = 1.65 
-weight = 84
+# height = 1.65 
+# weight = 84
 
-# Write your code here.
-# Calculate the bmi using weight and height.
-bmi =  weight / (height ** 2)
+# bmi =  weight / (height ** 2)
 
-print(bmi)
+# print(bmi)
+
+# print(round(bmi))
+
+# print(round(bmi, 2))
+
+bmi = 30.85
+height = 174
+weight = 90
+
+print(f"my height is {height} and weight is {weight}, based on the height and weight my bmi is {bmi}")
