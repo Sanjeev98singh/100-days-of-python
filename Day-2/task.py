0de74ac74785cd3be5acbf0857dec889545adc0a
Sanjeev <coolviccy123@gmail.com>
2024-08-09 14:59:32 +0530

    task.py updated

---
+++ Day-2/task.py
@@ -0,0 +1,43 @@
+print("Sanjeev"[6])
+print("Sanjeev"[-1])
+
+print("123" + "456")
+print(123 + 456)
+
+large integers
+print(123_456_789)
+
+float
+print(3.14536)
+
+boolean
+
+print(True)
+
+print(False)
+
+len("sanjeev")
+
+print(type("sanjit"))
+print(type(3.14))
+print(type(3))
+print(type(True))
+
+TYPE CONVERSION
+
+print(int("123") + int("456"))
+
+print(int(True))
+
+print("Number of letters in my name: " + str(len(input("Enter the name\n"))))
+
+
+print(5 // 3)
+print(5 / 3)
+print(5 ** 3)
+
+PEMDASLR - parenthesis,exponents,multiplication,/division and addition/substraction   
+
+print(3 * 3 + 3 / 3 - 3)
+print(3 * 3 + 3 // 3 - 3)
+print(3 * 3 + 3 // 3 - 7)
