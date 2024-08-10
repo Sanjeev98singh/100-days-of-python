368d2fc9d3d08dc1c92760ea5738e7ae3612b1f7
Sanjeev <coolviccy123@gmail.com>
2024-08-10 09:22:28 +0530

    append, title and capitalize methods added

---
+++ Day-2/main.py
@@ -0,0 +1,32 @@
+# user_input = "enter a todo list\n"
+
+# while True:
+#     todo = input(user_input)
+#     print(todo)
+#     print("Next todo list")
+
+# while True:
+#     todo = input(user_input)
+#     todos = [todo]
+#     print(todos)
+
+# todos = []
+# while True:
+#     todo = input(user_input)
+#     capital_Input = todo.capitalize()
+#     todos.append(capital_Input)
+#     print(todos)
+
+# while True:
+#     print("Enter todo list")
+
+
+# todos = []
+# while True:
+#     user_input = "enter a todo list\n"
+#     # not recommend to use string repeteadly     
+#     todo = input(user_input)
+#     # capital_Input = todo.capitalize()
+#     capital_Input = todo.title()
+#     todos.append(capital_Input)
+#     print(todos)    
