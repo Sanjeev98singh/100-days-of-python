0766ea069f3c72741f04074716abb6d7e82f247c
Sanjeev <coolviccy123@gmail.com>
2024-08-10 21:09:35 +0530

    conditional mechanism in python added

---
+++ Day-3/task.py
@@ -0,0 +1,89 @@
+# print("Welcome to the rollercoaster")
+
+# height = float(input("Enter your height in cm: "))
+
+# if height >= 120:
+#     print("You can ride the rollercoaster!")
+# else:
+#     print("Sorry, you are too short to ride the rollercoaster.")
+
+# print(10 % 3)
+
+# num = int(input("enter your number: "))
+
+# if num % 2 == 0:
+#     print("even")
+# else:
+#     print("odd")    
+
+# print("Welcome to the rollercoaster")
+
+# height = float(input("Enter your height in cm: "))
+
+# if height >= 120:
+#     print("You can ride the rollercoaster!")
+#     age = int(input("Enter your age: "))
+#     if age <= 10:
+#         print("You are not allowed to ride the rollercoaster due to age restriction.")
+#     elif age <= 18:
+#         print("You are allowed to ride the rollercoaster, but you will need a parent or guardian's permission.")
+#     else:
+#         print("You can ride the rollercoaster!")    
+# else:
+#     print("Sorry, you are too short to ride the rollercoaster.")
+
+
+# Q1------>
+# BMI Calculator with Interpretations
+# Add some if/elif/else statements to the BMI calculator so that it interprets the BMI values calculated.
+
+# If the bmi is under 18.5 (not including), print out "underweight"
+
+# If the bmi is between 18.5 (including) and 25 (not including), print out "normal weight"
+
+# If the bmi is 25 (including) or over, print out "overweight"
+
+# Refer to this graphic for help:
+
+# SOLUTION------>
+# weight = 85
+# height = 1.85
+
+# bmi = weight / (height ** 2)
+
+# if bmi < 18.5:
+#     print("underweight")
+    
+# elif 18.5 <= bmi < 25:
+#     print("normal weight")
+    
+# else:
+#     print("overweight")
+
+
+
+# print("Welcome to the rollercoaster")
+
+# height = float(input("Enter your height in cm: "))
+# bill = 0
+
+# if height >= 120:
+#     print("You can ride the rollercoaster!")
+#     age = int(input("Enter your age: "))
+#     if age <= 10:
+#         print("You are not allowed to ride the rollercoaster due to age restriction.")
+#     elif age <= 18:
+#         bill = 200
+#         print("You are allowed to ride the rollercoaster, but you will need a parent or guardian's permission.")
+#     else:
+#         bill = 300
+#         print("You can ride the rollercoaster!")  
+
+#     wants_photo_too = input("do you wants to click photo too? Type Y for yes and N for no: ")
+#     if wants_photo_too == "Y":
+#         bill += 100
+#         print("Total bill:", bill)      
+# else:
+#     print("Sorry, you are too short to ride the rollercoaster.")
+
+
