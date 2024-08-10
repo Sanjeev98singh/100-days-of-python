360a57cbaaab940c1d06e703220b5fa0a01bfd5a
Sanjeev <coolviccy123@gmail.com>
2024-08-10 10:29:46 +0530

    bugs practice updated

---
+++ Day-2/bugs.py
@@ -0,0 +1,31 @@
+# bug code
+# while True
+# print("Hello")
+
+# corrected code
+# while True:
+#     print("Hello")
+
+# bug code
+# greeting = "hello"
+# print(upper(greeting))
+
+# corrected code
+# greeting = "hello"
+# print(greeting.upper())
+
+# bug code
+# countries = []
+ 
+# while True:
+#     country = input("Enter the country: ")
+#     countries.append(country)
+# print(countries)
+
+# corrected code
+# countries = []
+ 
+# while True:
+#     country = input("Enter the country: ")
+#     countries.append(country)
+#     print(countries)
