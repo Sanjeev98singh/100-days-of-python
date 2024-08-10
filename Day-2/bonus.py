090daf73d0f7e6cbe2153fd2ebb1ce0b5acf1da2
Sanjeev <coolviccy123@gmail.com>
2024-08-10 09:35:29 +0530

    mini password protector console based app added

---
+++ Day-2/bonus.py
@@ -0,0 +1,7 @@
+# password = input("Enter password: ")
+# # will executed once
+
+# while password != "sanj123":
+#     password = input("Enter password: ")
+
+# print("password was correctly entered")
