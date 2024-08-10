50aab8456bf459d0364127ab8c864d5c85ae7930
Sanjeev <coolviccy123@gmail.com>
2024-08-10 09:54:59 +0530

    looping mechanism in python added

---
+++ Day-2/bonus2.py
@@ -0,0 +1,15 @@
+# num = 1
+
+# while num <= 10:
+#     print(num)
+#     num += 1
+
+# user_name = input("enter your name\n")
+
+# while True:
+#     print(user_name.capitalize()) #
+
+
+# while True:
+#     user_name = input("what is your name\n")
+#     print(user_name.capitalize()) #
