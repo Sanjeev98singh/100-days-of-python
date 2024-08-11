2d56cbf45399f59e7a823e48ee2ec01bf7245cd9
Sanjeev <coolviccy123@gmail.com>
2024-08-11 23:45:38 +0530

    base-converion-in-depth

---
+++ Day-4/base_conversion.py
@@ -0,0 +1,37 @@
+# commonly used python builtins functions. print(x) id(x)  type(x)
+# we can represent int values in multiple ways
+
+# decimal form(base-10)
+# binary form(base-2)
+# octal form(base-8)
+# hexa decimal form(base-16)
+
+# decimal form(base-10)
+
+# x = any_decimal_form
+
+# binary form(base-2)
+
+# binary_digit = 0B1111
+# binary_digit = 0b1111
+# print(binary_digit)
+
+# octal form(base-8)
+
+# octal_digit = 0o777
+# octal_digit = 0O777
+# print(octal_digit)
+
+# hexa decimal form(base-16)
+
+# hexa_digit = 0xFFF
+# hexa_digit = 0XFFF
+
+# print(hexa_digit)
+
+a =10
+b = 0B10
+c = 0O10
+d = 0X10
+
+print(a, b, c, d)
