f279964dea9650ad8fa10235fdd5fb4997caaeae
Sanjeev <coolviccy123@gmail.com>
2024-08-12 20:41:50 +0530

    day-5 practice added

--- Day-5/day-5.py
+++ Day-5/day-5.py
@@ -7,3 +7,14 @@
 # f = 1.2e3
 # f = 1.2E3
 # print(f)
+
+# complex data type
+
+# a + bj
+# complex_data_type = 10 + 20j
+# complex_data_type = 10.5 + 20.7j
+# print(complex_data_type)
+# print(type(complex_data_type))
+
+# complex_data_type = 0B1111 + 20j
+# print(complex_data_type)
