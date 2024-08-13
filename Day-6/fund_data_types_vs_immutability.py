980bfe945cd332789b4c2a31a0317a1d0ffb97c0
Sanjeev <coolviccy123@gmail.com>
2024-08-13 20:04:40 +0530

    fundamental data types vs immutability

--- Day-6/fund_data_types_vs_immutability.py
+++ Day-6/fund_data_types_vs_immutability.py
@@ -1,1 +1,15 @@
-print()
+# print(id(10))
+# print(id(11))
+
+# x = 10
+# print(id(x))
+# x+=1
+# print(id(x))
+
+# x = 10
+# y = x
+# print(id(x))
+# print(id(y))
+# y+=1
+# print(id(x))
+# print(id(y))
