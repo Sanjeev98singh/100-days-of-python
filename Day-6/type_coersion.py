d08a028b0c9cf02760161935caa459f6056b0d7b
Sanjeev <coolviccy123@gmail.com>
2024-08-13 17:54:14 +0530

    type_coersion updated

---
+++ Day-6/type_coersion.py
@@ -0,0 +1,10 @@
+# print(int('10'))
+# print(int('10.5'))
+# ValueError: invalid literal for int() with base 10: '10.5'
+# print(int(True))
+# print(int(10.5))
+# print(int(0B1111))
+# print(int('0B1111'))
+# ValueError: invalid literal for int() with base 10: '0B1111'
+# print(int('sanjeev'))
+# alueError: invalid literal for int() with base 10: 'sanjeev'
