9989841f61be3532ad0deb2a71d30374166007fd
Sanjeev <coolviccy123@gmail.com>
2024-08-13 18:35:37 +0530

    type_coersion updated

--- Day-6/type_coersion.py
+++ Day-6/type_coersion.py
@@ -1,3 +1,5 @@
+# int()
+
 # print(int('10'))
 # print(int('10.5'))
 # ValueError: invalid literal for int() with base 10: '10.5'
@@ -8,3 +10,15 @@
 # ValueError: invalid literal for int() with base 10: '0B1111'
 # print(int('sanjeev'))
 # alueError: invalid literal for int() with base 10: 'sanjeev'
+
+# float()
+
+# print(float(10))
+# print(float(10+20j))
+# TypeError: float() argument must be a string or a real number, not 'complex'
+# print(float(True))
+# print(float('10'))
+# print(float(True))
+# print(float('0B1111'))
+# ValueError: could not convert string to float: '0B1111'
+print(float(0B1111))
