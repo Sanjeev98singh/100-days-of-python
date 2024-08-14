2b790a4bd73cc4a6e2dfac568b932ac66a67e6a4
Sanjeev <coolviccy123@gmail.com>
2024-08-14 19:55:55 +0530

    immutabailty added

---
+++ Day-7/immutabilty.py
@@ -0,0 +1,11 @@
+# num1 = 10.5
+# num2 = 10.5
+# num1 = True
+# num2 = True
+# num1 = 'sanjeev'
+# num2 = 'vicky'
+# TypeError: id() takes exactly one argument (2 given)
+# print (id(num1))
+# print (id(num2))
+
+# print(num1 is num2) return 
