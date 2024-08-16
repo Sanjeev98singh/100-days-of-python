33f10088b05873aae8f915d3937264532d00a5f6
Sanjeev <coolviccy123@gmail.com>
2024-08-16 23:35:01 +0530

    tupple and set added

--- Day-9/tuple_and_set.py
+++ Day-9/tuple_and_set.py
@@ -7,3 +7,11 @@
 # print(type(t))
 
 # default numbers of values in the single variable will always be considered as tuple
+
+
+# sets
+
+s = {1,2,3,4}
+print(s)
+# print(s[1]) TypeError: 'set' object is not subscriptable
+print(type(s))
