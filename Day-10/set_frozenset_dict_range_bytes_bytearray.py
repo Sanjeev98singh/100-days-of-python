7426fc78f6d3b72df8ae969491c62ae92f5bfd1f
Sanjeev <coolviccy123@gmail.com>
2024-08-17 22:40:56 +0530

    range updated

--- Day-10/set_frozenset_dict_range_bytes_bytearray.py
+++ Day-10/set_frozenset_dict_range_bytes_bytearray.py
@@ -61,8 +61,13 @@
 
 # form-3
 
-r = range(1,11,2)
-# r = range(15,1,-2)
+# r = range(1,11,2)
+# # r = range(15,1,-2)
 
-for x in r:
-    print(x)
+# for x in r:
+#     print(x)
+
+# r  = range(10,5)
+
+# for x in r:
+#     print(x) nothing
