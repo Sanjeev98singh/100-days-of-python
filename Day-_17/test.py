c46ad2ba55bc354845648e53b4faf8f362a8a3f7
Sanjeev <coolviccy123@gmail.com>
2024-08-24 13:51:24 +0530

    real world examples of variable updated

--- Day-_17/test.py
+++ Day-_17/test.py
@@ -1,5 +1,23 @@
+# Example-1
+
 # dude can you suggest me that book from the library which you were talking about
 # Yeah sure that ook name was book_1
 # library = ['book_1', 'book_2', 'book_3', 'book_4']
 
 # print(library[0])
+
+# Example-2
+
+# dude can you give me brief description of the mobile device
+# mobile = {
+#     'name': 'iPhone 12',
+#     'price': 100000,
+#     'color': 'Black',
+#     'ram': '4GB',
+#     'storage': '64GB',
+#     'camera': '12MP'
+# }
+
+# print(mobile)
+# print(mobile['name'])
+
