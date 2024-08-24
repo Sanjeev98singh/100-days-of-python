ac9be395c05a8ea0e4893c116534281c03959ad7
Sanjeev <coolviccy123@gmail.com>
2024-08-24 13:45:57 +0530

    real world examples of variable updated

---
+++ Day-_17/test.py
@@ -0,0 +1,5 @@
+# dude can you suggest me that book from the library which you were talking about
+# Yeah sure that ook name was book_1
+# library = ['book_1', 'book_2', 'book_3', 'book_4']
+
+# print(library[0])
