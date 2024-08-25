56cbf4a54240684403dfe8531ed384099b38e749
Sanjeev <coolviccy123@gmail.com>
2024-08-25 16:18:36 +0530

    ord() function to analyse the ascii value updated

--- Day-_18/test.py
+++ Day-_18/test.py
@@ -3,3 +3,6 @@
 # print(ord('5')) 53
 # print(ord('a')) 97
 # print(ord('sanjeev')) TypeError: ord() expected a character, but string of length 7 found
+# print('sanjeev' > '297') 115 > 50
+# print(ord('s'))
+# print(ord('297'))
