64b7d129ec91beafb7cee73ff9ecd14681de8f52
Sanjeev <coolviccy123@gmail.com>
2024-08-29 21:38:07 +0530

    diff. betwn. == and is py file added

---
+++ Day-_21/test.py
@@ -0,0 +1,14 @@
+# == operator and is operator
+
+l1 = [1, 2, 3, 4, 5, 6, 7]
+l2 = [1, 2, 3, 4, 5, 6, 7]
+
+print(l1 == l2) 
+print(id(l1), id(l2))
+print(l1 is l2)
+
+l3 = l1
+
+print(l3 == l1)
+print(id(l3), id(l1))
+print(l3 is l1)
