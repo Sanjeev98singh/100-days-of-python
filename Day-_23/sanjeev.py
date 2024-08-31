d7c7a07d4d1de51d4410325a9933a7c61b6ad385
Sanjeev <coolviccy123@gmail.com>
2024-08-31 15:48:30 +0530

    module py file added

--- Day-_23/sanjeev.py
+++ Day-_23/sanjeev.py
@@ -1,8 +1,8 @@
-PI = 3.14
-name = 'sanjeev'
+# PI = 3.14
+# name = 'sanjeev'
 
 def add(a, b):
     print('sum:', a + b)
 
-def mult(a, b):
-    print('mult:', a * b)
+# def mult(a, b):
+#     print('mult:', a * b)
