8a34d9101d5e486951035d859ea737ecb84c4818
Sanjeev <coolviccy123@gmail.com>
2024-08-31 12:17:41 +0530

    ternery operator py file added

--- Day-_23/test.py
+++ Day-_23/test.py
@@ -11,3 +11,10 @@
 # num1,num2 = 10,20
 # num3 = 30 if num1 < num2 else 40
 # print(num3)
+
+# num1 = int(input('Enter First Number'))
+# num2 = int(input('Enter Second Number'))
+
+# min = num1 if num1 < num2 else num2
+
+# print('printed minimum value: ',min)
