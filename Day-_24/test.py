8c20089eacc1e0bae08d47cfd4c8d8dc01099023
Sanjeev <coolviccy123@gmail.com>
2024-09-02 11:37:48 +0530

    input py file added

--- Day-_24/test.py
+++ Day-_24/test.py
@@ -4,5 +4,6 @@
 # value = int(input('Enter The Value: '))
 # print(type(value))
 
-value = int(input('Enter The Value: '))
-print(type(value))
+# value = float(input('Enter The Value: '))
+# print(value)
+# print(type(value))
