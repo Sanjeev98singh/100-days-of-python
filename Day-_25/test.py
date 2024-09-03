59c943ade37e788656180a3007aa502caeb4ddff
Sanjeev <coolviccy123@gmail.com>
2024-09-03 12:24:29 +0530

    CLA py file added

--- Day-_25/test.py
+++ Day-_25/test.py
@@ -7,3 +7,4 @@ from sys import argv
 # print(type(argv)) <class 'list'>
 print('The number of command line arguments: ',len(argv))   #python test.py 10 20 30 ->  The number of command line arguments:  4
 print('The number of command line arguments: ',argv)       #The number of command line arguments:  ['test.py', '10', '20', '30']
+print(f'First argument {argv[0]}, second argument {argv[1]}, third argument {argv[2]}, fourth argument {argv[3]}')
