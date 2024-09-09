da3cddc1d4430f0924958f74dc2e657078396eb1
Sanjeev <coolviccy123@gmail.com>
2024-09-09 15:56:16 +0530

    formatted string py file added

--- Day-_27/test.py
+++ Day-_27/test.py
@@ -11,3 +11,7 @@
 
 # val1 = 10
 # print('val1 value is %i' %val1) val1 value is 10
+
+# val_1,val_2,val_3 = 10,20,30
+
+# print('The values are %i,%i,%i' %(val_1,val_2,val_3)) The values are 10,20,30
