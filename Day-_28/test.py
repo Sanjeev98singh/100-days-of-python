315384a3d678b8667ee263a2b8097d29197c00c3
Sanjeev <coolviccy123@gmail.com>
2024-09-10 19:34:30 +0530

    indentation py file added

--- Day-_28/test.py
+++ Day-_28/test.py
@@ -2,6 +2,6 @@
 #     print('sanjeev')
 #     print('san')
 
-for i in range(10):
-    print('sanjeev')
+# for i in range(10):
+#     print('sanjeev')
     #  print('san') IndentationError: unexpected indent
