6a58fc938a82eb61b3bdf368807b27f6497ca8ab
Sanjeev <coolviccy123@gmail.com>
2024-09-11 18:57:03 +0530

    membership operator in coditional operator py file added

--- Day-_29/test.py
+++ Day-_29/test.py
@@ -8,3 +8,6 @@
 val = int(input('Enter a digit from 0 to 9: '))
 units = {0:'zero', 1:'one', 2:'two', 3:'three', 4:'four', 5:'five', 6:'six', 7:'seven', 8:'eight', 9:'nine'}
 tens  = {2:'twenty', 3:'thirty', 4:'forty', 5:'fifty', 6:'sixty', 7:'seventy', 8:'eighty', 9:'ninety'}
+
+ones_digit = val%10
+tens_digit = val//10
