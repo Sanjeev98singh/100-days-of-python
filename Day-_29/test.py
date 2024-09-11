85a5b35e0df654df6d6b29181291ba3a8424b224
Sanjeev <coolviccy123@gmail.com>
2024-09-11 18:19:56 +0530

    membership operator in coditional operator py file added

---
+++ Day-_29/test.py
@@ -0,0 +1,6 @@
+val = int(input('Enter a digit from 0 to 9: '))
+d = {0:'zero', 1:'one', 2:'two', 3:'three', 4:'four', 5:'five', 6:'six', 7:'seven', 8:'eight', 9:'nine'}
+if(val in d):
+    print(d[val])
+else:
+    print('Invalid input. Please enter a digit from 0 to 9.')
