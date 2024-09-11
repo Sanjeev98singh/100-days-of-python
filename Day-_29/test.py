a890f96cc49d5d3b12d63608a66e521ea24768ab
Sanjeev <coolviccy123@gmail.com>
2024-09-11 19:09:39 +0530

    membership operator in coditional operator py file added

--- Day-_29/test.py
+++ Day-_29/test.py
@@ -5,12 +5,12 @@
 # else:
 #     print('Invalid input. Please enter a digit from 0 to 9.')
 
-val = int(input('Enter any two digits number: '))
-units = {0:'zero', 1:'one', 2:'two', 3:'three', 4:'four', 5:'five', 6:'six', 7:'seven', 8:'eight', 9:'nine'}
-tens  = {2:'twenty', 3:'thirty', 4:'forty', 5:'fifty', 6:'sixty', 7:'seventy', 8:'eighty', 9:'ninety'}
+# val = int(input('Enter any two digits number: '))
+# units = {0:'zero', 1:'one', 2:'two', 3:'three', 4:'four', 5:'five', 6:'six', 7:'seven', 8:'eight', 9:'nine'}
+# tens  = {2:'twenty', 3:'thirty', 4:'forty', 5:'fifty', 6:'sixty', 7:'seventy', 8:'eighty', 9:'ninety'}
 
-ones_digit = val%10
-tens_digit = val//10
+# ones_digit = val%10
+# tens_digit = val//10
 
-output = tens[tens_digit] + " " + units[ones_digit]
-print(output)
+# output = tens[tens_digit] + " " + units[ones_digit]
+# print(output)
