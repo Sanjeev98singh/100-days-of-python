fe7c6e98b82ebeda39f61e10bb829e297152fbd0
Sanjeev <coolviccy123@gmail.com>
2024-09-11 18:54:05 +0530

    membership operator in coditional operator py file added

--- Day-_29/test.py
+++ Day-_29/test.py
@@ -7,7 +7,4 @@
 
 val = int(input('Enter a digit from 0 to 9: '))
 units = {0:'zero', 1:'one', 2:'two', 3:'three', 4:'four', 5:'five', 6:'six', 7:'seven', 8:'eight', 9:'nine'}
-if(val in d):
-    print(d[val])
-else:
-    print('Invalid input. Please enter a digit from 0 to 9.')
+tens  = {2:'twenty', 3:'thirty', 4:'forty', 5:'fifty', 6:'sixty', 7:'seventy', 8:'eighty', 9:'ninety'}
