48422f7ad8d870dfa4b4dc36e1219ae307f1cd4d
Sanjeev <coolviccy123@gmail.com>
2024-09-13 12:08:09 +0530

    py file added

--- Day-_30/test.py
+++ Day-_30/test.py
@@ -7,3 +7,18 @@
 #     print(f'{number1} is the smallest number.')
 # else: 
 #     print(f'{number2} is the smallest number.')
+
+# smallest given 3 numbers
+
+# number1 = int(input('Enter the 1st number bro: '))
+
+# number2 = int(input('Enter the 2nd number bro: '))
+
+# number3 = int(input('Enter the 3rd number bro: '))
+
+# if(number1 < number2 and number1 < number3):
+#     print(f'{number1} is the smallest number.')
+# elif number2 < number3:
+#     print(f'{number2} is the smallest number.')
+# else:
+#     print(f'{number3} is the smallest number.')         
