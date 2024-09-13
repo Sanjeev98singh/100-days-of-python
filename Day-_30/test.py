bc2088d34b02b0813bdc7fe45a770bb3b603428a
Sanjeev <coolviccy123@gmail.com>
2024-09-13 11:10:16 +0530

    py file added

---
+++ Day-_30/test.py
@@ -0,0 +1,9 @@
+# smallest given 2 numbers
+
+# number1 = int(input('Enter the 1st number bro: '))
+# number2 = int(input('Enter the 2nd number bro: '))
+
+# if(number1 < number2):  
+#     print(f'{number1} is the smallest number.')
+# else: 
+#     print(f'{number2} is the smallest number.')
