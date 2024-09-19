8a71390163e68e938b974e5676a438fd7fa89bbc
Sanjeev <coolviccy123@gmail.com>
2024-09-19 19:49:33 +0530

    none data type py file added

--- Day-_33/test.py
+++ Day-_33/test.py
@@ -2,3 +2,21 @@
 #     a = 10
 
 # print(m1()) none   
+
+# d  = {}
+
+# print(d)
+# print(type(d))
+
+# d[1] = 'sanjeev'
+# d[2] = 'm1'
+
+# print(d)
+
+# d['yo'] = '10'
+
+# print(d)
+
+# d['yo'] = 10
+
+# print(d)
