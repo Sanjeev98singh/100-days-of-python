7d3ad29228868331f5a629dd88c13e31e4aa8097
Sanjeev <coolviccy123@gmail.com>
2024-09-30 11:53:18 +0530

    multiple value input in single line py file added

--- Day-_35/test.py
+++ Day-_35/test.py
@@ -3,5 +3,13 @@
 
 # a = [int(x) for x in input('enter the number buddy:').split()]
 # print(a) [1,2]
-a, b = [int(x) for x in input('enter the number buddy:').split(' ')]
-print(a, b)
+# a, b = [int(x) for x in input('enter the number buddy:').split(' ')]
+# print(a, b)
+
+# number = eval(input('enter the number'))
+# print(number)
+# print(type(number))
+
+# number = eval("1+2+3")
+# print(number)
+# print(type(number))
