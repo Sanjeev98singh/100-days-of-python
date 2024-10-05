4b12a9e989c7c359841f209333fa53b4d1f89863
Sanjeev <coolviccy123@gmail.com>
2024-10-05 19:10:30 +0530

    pattern py file added

---
+++ Day-_37/test.py
@@ -0,0 +1,7 @@
+# value = int(input('enter the number'))
+# for i in range(value):
+#     print('*',end=' ')
+# print()
+# value = int(input('enter the number'))
+# for i in range(value):
+#     print('* ',end='')    
